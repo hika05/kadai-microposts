327bb5a1f2d50cebb6645fd7bf35b090c0c7e545
hikari <rinrinm05@gmail.com>
2021-11-28 13:11:53 +0000

    follow, unfollow

--- app/User.php
+++ app/User.php
@@ -107,7 +107,7 @@ class User extends Authenticatable
      */
     public function loadRelationshipCounts()
     {
-        $this->loadCount('microposts');
+       $this->loadCount(['microposts', 'followings', 'followers']);
     }
 
     protected $fillable = [
